8bfbf1b53867215afdab6104e8e4578733cdb0f0
berkecengiz <berkecengiz94@gmail.com>
2022-05-17 09:32:49 +0200

    Heroku erros solved, run.py add

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 from datetime import timedelta
-from db import db
 
 app = Flask(__name__)
 app.secret_key = 'secretkey12345'
@@ -35,4 +34,10 @@ api.add_resource(UserRegister, '/register')            # http://127.0.0.1/regist
 
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    from db import db
+    db.init_app(app)
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
